e0dc98cec44f9e826aa8a92edc055586737b6472
elyoel <yoelcaldelderonc@gmail.com>
2026-04-02 17:15:22 -0300

    Ejercicio 3 corregido

--- Practica 2/src/ejercicio3.py
+++ Practica 2/src/ejercicio3.py
@@ -1,23 +1,22 @@
 def spoiler ():
-    review = """La película sigue a un grupo de astronautas que
-    viajan a Marte
+    review = """    La película sigue a un grupo de astronautas que viajan a Marte
     en una misión de rescate. El capitán Torres lidera al equipo
-    a través
-    de tormentas solares y fallos en el sistema de navegación. Al
-    llegar
-    a Marte descubren que la base está abandonada y los
-    suministros
-    destruidos. Torres decide sacrificar la nave nodriza para
-    salvar
-    al equipo y logran volver a la Tierra en una cápsula de
-    emergencia.
-    El final revela que Torres sobrevivió gracias a un pasaje
-    secreto."""
+    a través de tormentas solares y fallos en el sistema de navegación. Al llegar
+    a Marte descubren que la base está abandonada y los suministros
+    destruidos. Torres decide sacrificar la nave nodriza para salvar
+    al equipo y logran volver a la Tierra en una cápsula de emergencia.
+    El final revela que Torres sobrevivió gracias a un pasaje secreto."""
 
-    words = input('Ingrese las palabras spoiler (separadas por coma): ').split(', ')
-    words_review = review.split()
+    spoiler_words = input('Ingrese las palabras spoiler (separadas por coma): ').lower().split(', ')
+    review_words = review.split('\n')
 
-    words_review = ['*' * len(word) if word in words else word for word in words_review]
 
-    words_review = ' '.join(words_review)
-    print(words_review)
+    for i, line in enumerate(review_words):
+        new_line = line
+        for word in line.split():
+            if word.lower().strip('. ') in spoiler_words:
+                new_line = new_line.replace(word, '*' * len(word))
+        review_words[i] = new_line
+
+    review_words = '\n'.join(review_words)
+    print(review_words)
